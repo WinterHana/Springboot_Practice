9cdd7dad632916db5eb96d3a8f7d0c502ee04f38
Winterhana <haha920@naver.com>
2025-02-19 20:46:07 +0900

    feat : 250219 Study

---
+++ GuestBookPractice/src/test/java/org/zerock/guestbookpractice/GuestBookPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.guestbookpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestBookPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
